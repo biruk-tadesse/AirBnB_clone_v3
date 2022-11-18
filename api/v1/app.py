a7c6a3a1c5a1f7c5a1eb3ccd1683869fff8ea516
Biruk <biruk.tadesse.tefera@gmail.com>
2022-11-18 15:02:32 +0300

    storage_get_count branch

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,8 @@
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import Flask, make_response, jsonify
+from flask import Flask
 from flask import Blueprint
-from flask_cors import CORS
 
 """Host and port env variables"""
 host_env = getenv('HBNB_API_HOST') or '0.0.0.0'
@@ -14,8 +13,6 @@ port_env = getenv('HBNB_API_PORT') or 5000
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-""" Cors access to selected resources from a different origin."""
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -23,19 +20,7 @@ def teardown_db(error):
     """ Close db session """
     storage.close()
 
-
-@app.errorhandler(404)
-def not_found(error):
-    """
-    Method to manage the url's that does'nt exist
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
-
-
 if __name__ == "__main__":
-    """
-    Main Function
-    """
     app.run(
         host=host_env, port=port_env,
         debug=True, threaded=True,
